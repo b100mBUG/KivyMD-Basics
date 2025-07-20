81da821fc08b63d204dbee96342a36cbce40a3d5
Fidel Were <werefidelcastro@outlook.com>
2025-07-20 21:22:55 +0300

    Update main.py
    KivyMD 2.0.0 syntax has changed. so, we need to also change.

--- 13 - Switching Screens/main.py
+++ 13 - Switching Screens/main.py
@@ -1,48 +1,51 @@
-from kivymd.app import MDApp
-from kivy.lang.builder import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-
+# KivyMD changed. Now, lets do it in kivymd 2.0.0
+from kivymd.app import MDApp # This is cool...
+from kivy.lang.builder import Builder # This is also cool
+from kivy.uix.screenmanager import ScreenManager, Screen # This is also cool, but you could use kivymd.uix.screen import MDScreenManager...
+# Now, Lets do it the easier way. 
 screen_helper = """
 
-ScreenManager:
-    MenuScreen:
-    ProfileScreen:
-    UploadScreen:
+MDScreenManager:
+    id: sm
 
-<MenuScreen>:
-    name: 'menu'
-    MDRectangleFlatButton:
-        text: 'Profile'
-        pos_hint: {'center_x':0.5,'center_y':0.6}
-        on_press: root.manager.current = 'profile'
-    MDRectangleFlatButton:
-        text: 'Upload'
-        pos_hint: {'center_x':0.5,'center_y':0.5}
-        on_press: root.manager.current = 'upload'
+    MDScreen: # change it from class name, to just calling the class directly in kv file.
+        name: 'menu'
+        MDButton:
+            pos_hint: {'center_x':0.5,'center_y':0.6}
+            on_press: sm.current = 'profile'
+            MDButtonText:
+                text: 'Profile'
+        MDButton:
+            pos_hint: {'center_x':0.5,'center_y':0.5}
+            on_press: sm.current = 'upload'
+            MDButtonText:
+                text: 'Upload'
     
-<ProfileScreen>:
-    name: 'profile'
-    MDLabel:
-        text: 'Profile'
-        halign: 'center'
-    MDRectangleFlatButton:
-        text: 'Back'
-        pos_hint: {'center_x':0.5,'center_y':0.1}
-        on_press: root.manager.current = 'menu'
+    MDScreen:
+        name: 'profile'
+        MDLabel:
+            text: 'Profile'
+            halign: 'center'
+        MDButton:
+            pos_hint: {'center_x':0.5,'center_y':0.1}
+            on_press: sm.current = 'menu'
+            MDButtonText:
+                text: 'Back'
         
-<UploadScreen>:
-    name: 'upload'
-    MDLabel:
-        text: 'Upload'
-        halign: 'center'
-    MDRectangleFlatButton:
-        text: 'Back'
-        pos_hint: {'center_x':0.5,'center_y':0.1}
-        on_press: root.manager.current = 'menu'
+    MDScreen:
+        name: 'upload'
+        MDLabel:
+            text: 'Upload'
+            halign: 'center'
+        MDButton:
+            pos_hint: {'center_x':0.5,'center_y':0.5}
+            on_press: sm.current = 'menu'
+            MDButtonText:
+                text: 'Back'
         
 """
-
-
+# Now, we do away with the weird classes.
+"""
 class MenuScreen(Screen):
     pass
 
@@ -53,15 +56,17 @@ class ProfileScreen(Screen):
 
 class UploadScreen(Screen):
     pass
+"""
 
-
+# we already defined this in our kv file, so, we gonna do away with it also.
+"""
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(ProfileScreen(name='profile'))
 sm.add_widget(UploadScreen(name='upload'))
 
-
+"""
 class DemoApp(MDApp):
 
     def build(self):
@@ -70,3 +75,4 @@ class DemoApp(MDApp):
 
 
 DemoApp().run()
+# Yeah, cool. there you go.
